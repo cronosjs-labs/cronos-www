7b394aae8064faf60af3f276e7ab7efe92110341
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2023-11-16 16:59:21 -0600

    Add files via upload

---
+++ rspack.config.js
@@ -0,0 +1,44 @@
+const rspack = require("@rspack/core");
+
+const config = {
+  entry: {
+    main: "./src/main.tsx",
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    allowedHosts: "all",
+    host: "localhost",
+    port: 5173,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: {
+                  tailwindcss: {},
+                  autoprefixer: {},
+                },
+              },
+            },
+          },
+        ],
+        type: "css",
+      },
+    ],
+  },
+  plugins: [new rspack.HtmlRspackPlugin({ template: "./index.html" })].filter(
+    Boolean
+  ),
+};
+
+module.exports = config;
